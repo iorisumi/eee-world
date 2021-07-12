b1698595a7a50b6fadcf02c280c03e086fd797be
iorisumi <84116084+iorisumi@users.noreply.github.com>
2021-07-12 12:26:20 +0900

    Add files via upload

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var world = require('./js/server_world');
+var world = require('./api/server_world');
 
 app.get('/', (req,res)=>{
 	res.sendFile(__dirname + '/index.html');
