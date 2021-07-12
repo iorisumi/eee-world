d53785c2fa857d966f5650bcd0416ceb76dfb279
iorisumi <84116084+iorisumi@users.noreply.github.com>
2021-07-12 14:54:28 +0900

    Update app.js

--- api/app.js
+++ api/app.js
@@ -56,9 +56,8 @@ io.on('connection', (socket)=>{
 	});
 });
 
-var port = process.env.API_URL || 8080;
-var ip_address = process.env.API_URL || '127.0.0.1';
+var port = process.env.PORT || 8080;
 
-http.listen(port, ip_address, ()=>{
-	console.log("listening on " + ip_address + ", server_port" + port);
+http.listen(port, ()=>{
+	console.log("server_port" + port);
 });
