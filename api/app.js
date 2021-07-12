204c8b2fc72ea0b20dcdd8bed821e26ce8199e9c
iorisumi <84116084+iorisumi@users.noreply.github.com>
2021-07-12 23:03:33 +0900

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var world = require('server_world.js');
+var world = require('./server_world');
 
 
 app.get('/', (req,res)=>{
